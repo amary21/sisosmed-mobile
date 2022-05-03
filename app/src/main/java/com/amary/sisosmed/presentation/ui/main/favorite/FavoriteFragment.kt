dfa005b766b40b8fb1d5238139d69c8c5bcd522f
Taufik Amaryansyah <taufik.amary@gmail.com>
2022-05-03 18:42:56 +0700

    feat: clean up code

--- app/src/main/java/com/amary/sisosmed/presentation/ui/main/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/amary/sisosmed/presentation/ui/main/favorite/FavoriteFragment.kt
@@ -2,13 +2,8 @@ package com.amary.sisosmed.presentation.ui.main.favorite
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.amary.sisosmed.base.BaseFragment
 import com.amary.sisosmed.databinding.FragmentFavoriteBinding
-import com.amary.sisosmed.presentation.adapter.PagerAdapter
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate) {
